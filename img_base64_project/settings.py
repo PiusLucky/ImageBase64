39b53bf6f208e83763ce1ee354b7af4799d928c1
luckypius50 <luckypius50@gmail.com>
2020-09-22 05:22:04 +0100

    :rocket:

--- img_base64_project/settings.py
+++ img_base64_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
 
