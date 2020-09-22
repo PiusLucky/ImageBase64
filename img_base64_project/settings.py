205cfa243187e68209fb74fa1f07c2cca0644504
luckypius50 <luckypius50@gmail.com>
2020-09-22 04:59:41 +0100

    :rocket:

--- img_base64_project/settings.py
+++ img_base64_project/settings.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 import django_heroku
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = os.environ['DATA_UPLOAD_MAX_MEMORY_SIZE']
+DATA_UPLOAD_MAX_MEMORY_SIZE = int(os.environ['DATA_UPLOAD_MAX_MEMORY_SIZE'])
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
