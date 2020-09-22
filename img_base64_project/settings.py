c1b839ac9f5cf8376ca4766248a08812637f069b
luckypius50 <luckypius50@gmail.com>
2020-09-22 04:49:02 +0100

    :rocket:

--- img_base64_project/settings.py
+++ img_base64_project/settings.py
@@ -87,7 +87,7 @@ ROOT_URLCONF = 'img_base64_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + '/templates/'],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
