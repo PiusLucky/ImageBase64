87ee5336393d1e861a57cf2248dd6967263dfcc2
luckypius50 <luckypius50@gmail.com>
2020-09-22 04:52:15 +0100

    :rocket:

--- img_base64_project/settings.py
+++ img_base64_project/settings.py
@@ -9,9 +9,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
+
+ADMINS = (
+    ("Pius", os.environ['ADMIN_EMAIL']),
+)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
